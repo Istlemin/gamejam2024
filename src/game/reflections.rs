f90de84ab7f046e18cbc28f64b31aae78d9416b2
Joël Huber <mail@johutha.ch>
2024-02-10 23:45:42 +0000

    Fix bullet rotation

--- src/game/reflections.rs
+++ src/game/reflections.rs
@@ -110,17 +110,15 @@ fn mirror_reflect_bullets(
     for event in reflection_event_reader.read() {
         bullets.for_each_mut(|(_, mut transform, mut velocity)| {
             let pos = transform.translation.xy();
-            let rot_up = transform.up().xy();
             let velo_2d = velocity.linvel;
             let line = event.mirror.get_line();
 
             if event.mirror.on_strip(pos) {
                 let new_pos = pos.reflect_over_line(line).extend(0.0);
                 let new_velo = velo_2d.reflect_over_line(line.centered_line());
-                let new_up = rot_up.reflect_over_line(line.centered_line()).extend(0.0);
 
                 transform.translation = new_pos;
-                transform.look_at(new_pos + new_velo.extend(0.0), new_up);
+                transform.rotation = Quat::from_rotation_z(Vec2::X.angle_between(new_velo));
                 velocity.linvel = new_velo;
             }
         })
